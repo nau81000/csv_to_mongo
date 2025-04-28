c8e9a87c582b2aa3bc4577ea00aedae3f5a552a8
Nicolas Audheon <audheon.nicolas@gmail.com>
2025-04-28 07:27:16 +0200

    improve tests

--- tests/test_migration.py
+++ tests/test_migration.py
@@ -9,35 +9,26 @@ from migration import build_df, insert_df_to_mongo, check_indexes
 
 class TestMigration:
 
-    def test_df_construction(self):
-        """ Test de la bonne construction du dataframe
+    def setup_class(cls):
+        """ Initialise l'environnement et le dataframe
         """
         # Chargement de l'environnement
         load_dotenv()
         # Construction du dataframe
-        build_df(os.getenv('CSV_DATASET_FILENAME'))
-        assert True
+        cls.dataframe = build_df(os.getenv('CSV_DATASET_FILENAME'))
 
     def test_df_valeurs_manquantes(self):
         """ Test qu'il n' y a aucune valeur manquante
         """
-        # Chargement de l'environnement
-        load_dotenv()
-        # Construction du dataframe
-        dataframe = build_df(os.getenv('CSV_DATASET_FILENAME'))
         # Test qu'il n' y a aucune valeur manquante
-        assert not dataframe.isna().any().any()
+        assert not self.dataframe.isna().any().any()
 
     def test_mongo_insert(self):
-        """ Test de l'insertion du dataframe dans une Mongo
+        """ Test de l'insertion du dataframe dans une base Mongo
         """
-        # Chargement de l'environnement
-        load_dotenv()
-        # Construction du dataframe
-        dataframe = build_df(os.getenv('CSV_DATASET_FILENAME'))
         # Vérification des indexes
-        indexes = check_indexes(dataframe, os.getenv('INDEXES'))
+        indexes = check_indexes(self.dataframe, os.getenv('INDEXES'))
         # Insertion du dataframe dans une base MongoDB
-        result = insert_df_to_mongo(dataframe, os.getenv('DB_SERVER'), os.getenv('DB_NAME'), os.getenv('COLLECTION_NAME'), indexes)
+        result = insert_df_to_mongo(self.dataframe, os.getenv('DB_SERVER'), os.getenv('DB_NAME'), os.getenv('COLLECTION_NAME'), indexes)
         # Test que le dataframe et la base de données ont le même nombre d'enregistrements
-        assert result == len(dataframe)
+        assert result == len(self.dataframe)
